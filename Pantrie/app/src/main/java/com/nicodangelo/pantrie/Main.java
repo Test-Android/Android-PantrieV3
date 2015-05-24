266678f083def050fecec9b67d23c6c418c72b8b
Jake Cox <nicodangelo15@gmail.com>
2015-05-24 18:02:16 -0500

    added in most needed Items
    This version of Pantrie will be much simpler than the other ones, along
    with much more user friendly:)

--- Pantrie/app/src/main/java/com/nicodangelo/pantrie/Main.java
+++ Pantrie/app/src/main/java/com/nicodangelo/pantrie/Main.java
@@ -1,9 +1,8 @@
 package com.nicodangelo.pantrie;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class Main extends ActionBarActivity
@@ -14,31 +13,32 @@ public class Main extends ActionBarActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
+        Runnable r = new Runnable()
         {
-            return true;
-        }
+            @Override
+            public void run()
+            {
+                try
+                {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
 
-        return super.onOptionsItemSelected(item);
+                Intent i = new Intent(Main.this, Home.class);
+                startActivity(i);
+            }
+        };
+
+        Thread theThread = new Thread(r);
+        theThread.start();
     }
+
+    @Override
+    public void onBackPressed(){}
+
 }
---
+++ Pantrie/app/src/main/java/com/nicodangelo/util/Settings.java
@@ -0,0 +1,58 @@
+package com.nicodangelo.util;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.nicodangelo.pantrie.Home;
+import com.nicodangelo.pantrie.R;
+
+
+public class Settings extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+
+    public void launchConversionCalc(View view) {
+        Intent i = new Intent(this, ConvertMass.class);
+        startActivity(i);
+    }
+
+    //TODO:add jetts game(cleaner version)
+    /*public void startGame(View view) {
+        Intent i = new Intent(this, ArrowGame.class);
+        startActivity(i);
+    }*/
+
+    @Override
+    public void onBackPressed() {
+        Toast toast = Toast.makeText(getApplicationContext(), "There is no going back now:)", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void goBack(View view)
+    {
+        Intent i = new Intent(this, Home.class);
+        startActivity(i);
+    }
+
+    //TODO:fix Database Handler
+    /*public void deleteDB(View view)
+    {
+        DBHandler db = new DBHandler(this);
+        db.open();
+        db.deleteAll();
+        db.close();
+        Intent i = new Intent(Settings.this, ListMain.class);
+        startActivity(i);
+    }*/
+}
+
