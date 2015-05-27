fd7794cc5867036bea11e64d7e886b2d9dca1a8b
Jake Cox <nicodangelo15@gmail.com>
2015-05-27 07:55:50 -0500

    Started the creation of the database variables

--- Pantrie/app/src/main/java/com/nicodangelo/database/DatabaseHandler.java
+++ Pantrie/app/src/main/java/com/nicodangelo/database/DatabaseHandler.java
@@ -1,3 +1,5 @@
+//@author Jake Cox (so you no who to blame)
+
 package com.nicodangelo.database;
 
 import android.content.Context;
@@ -6,6 +8,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHandler extends SQLiteOpenHelper
 {
+    //Logcat tag : I really dont know what this is:)
+    private static final String LOG = "DatabaseHandler";
+
+    //Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    //Database Name
+    private static final String DATABASE_NAME = "pantrie_database";
+
+    //Table Names
+    private static final String TABLE_SETTINGS = "settings";
+    private static final String TABLE_ITEMS_MAIN = "items_main";
+    private static final String TABLE_ITEMS_GROCERY = "items_grocery";
 
     public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
     {
